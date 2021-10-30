87a89e2eb3e4c88bd00727a6c3d1a9b8e4c44edc
Annalisa <annalisa.bauer1@gmail.com>
2021-10-29 20:21:55 -0700

    updated backend part of front stopped at Pages

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,72 @@
+const { AuthenicationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v-password"
+        );
+        return userData;
+      }
+      addEventListener: throw new AuthenicationError("not logged in");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenicationError("Incorrect user");
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw new AuthenicationError("Incorrect password");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updated = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+
+        return updated;
+      }
+      throw new AuthenicationError("you need to log in");
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updated = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        return updated;
+      }
+      throw new AuthenicationError("you need to log in");
+    },
+  },
+};
+
+module.exports = resolvers;
